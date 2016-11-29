e1a6a6bb318682757a92d60f7a470e5fa9951bb7
Nikita Golovin <golovin.nik@gmail.com>
2016-11-29 22:16:11 +0300

    slides

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,16 +1,55 @@
 <template lang="pug">
   .home
     .index-view
-      .screen
-      .screen
-      .screen
-      .screen
+      .cont(
+        v-for="(slide, index) of slides"
+        v-bind:key="slide"
+        )
+        transition(name="slide")
+          .screen(v-show="index == slideNum" v-bind:style="{ backgroundImage: 'url(' + path + slide + ')' }")
 </template>
 
 <script>
+  const path = 'assets/img/slides/';
+  const slides = [
+    'slide1.jpg',
+    'slide2.jpg',
+    'slide3.jpg'
+  ];
+  
   export default {
-    name: "HomeComponent"
+    name: "HomeComponent",
+    
+    data: function () {
+      return {
+        path,
+        slides,
+        slideNum: 0
+      }
+    },
+  
+    mounted: function () {
+      setInterval(() => this.slideNext(), 5000);
+    },
+    
+    methods: {
+      slideNext: function () {
+        if (this.slideNum >= slides.length - 1)
+          this.slideNum = 0;
+        else
+          this.slideNum++;
+      },
+  
+      slidePrev: function () {
+        if (this.slideNum == 0)
+          this.slideNum = slides.length - 1;
+        else
+          this.slideNum--;
+      }
+    }
   }
+  
+  
 </script>
 
 <style lang="scss" scoped rel="stylesheet/scss">
@@ -20,28 +59,30 @@
     right: 0;
     bottom: 0;
     left: 0;
-    margin-left: 216px;
   
     .screen {
       position: absolute;
       top: 0;
-      left: 0;
+      left: 175px;
       width: 100%;
       height: 100%;
-      transition: trasform .3s ease;
-      &:nth-child(4n-3) {
-        background: red;
-      }
-      &:nth-child(4n-2) {
-        background: green;
-      }
-      &:nth-child(4n-2) {
-        background: blue;
-      }
-      &:nth-child(4n-1) {
-        background: pink;
-      }
+      background: center center no-repeat / cover;
     }
   }
+</style>
+
+<style lang="sss" scoped rel="stylesheet/sass">
+  .slide-enter-active
+    transition: opacity 1s ease, transform 4s ease
+    
+  .slide-leave-active
+    transition: opacity 1s ease-in, transform 4s ease
+  
+  .slide-enter
+    opacity: 0
+    transform: translate3d(-175px, 0, 0)
+  
+  .slide-leave-active
+    opacity: 0
   
 </style>
